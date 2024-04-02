4e958d1bb6d838ec25eadd812b7bf2f89928dac9
alfasierraconnects <alfasierraconnects@gmail.com>
2024-04-02 23:07:45 +0530

    Routed using React Router

--- react-restaurant-website/src/App.js
+++ react-restaurant-website/src/App.js
@@ -1,11 +1,20 @@
 import React from "react";
 import Navbar from "./components/Navbar/Navbar";
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import Cart from "./pages/Cart";
+import PlaceOrder from "./pages/PlaceOrder";
 
 const App = () => {
   return (
     <div className="min-h-screen">
       <div className="w-[80%] m-auto">
         <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/cart" element={<Cart />} />
+          <Route path="/order" element={<PlaceOrder />} />
+        </Routes>
       </div>
     </div>
   );

710ed0f1e4b61323d14533fc248681b1f4aaa9fb
alfasierraconnects <alfasierraconnects@gmail.com>
2024-04-04 01:31:36 +0530

    Added Footer

--- react-restaurant-website/src/App.js
+++ react-restaurant-website/src/App.js
@@ -4,19 +4,23 @@ import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import Cart from "./pages/Cart";
 import PlaceOrder from "./pages/PlaceOrder";
+import Footer from "./components/Footer/Footer";
 
 const App = () => {
   return (
-    <div className="min-h-screen">
-      <div className="w-[80%] m-auto">
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/cart" element={<Cart />} />
-          <Route path="/order" element={<PlaceOrder />} />
-        </Routes>
+    <>
+      <div className="App min-h-screen">
+        <div className="w-[80%] m-auto">
+          <Navbar />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/cart" element={<Cart />} />
+            <Route path="/order" element={<PlaceOrder />} />
+          </Routes>
+        </div>
       </div>
-    </div>
+      <Footer />
+    </>
   );
 };
 

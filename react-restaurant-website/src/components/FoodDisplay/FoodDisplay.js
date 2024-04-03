c9a5351dd3250e5b6991ee0ba5adc60a4f87b128
alfasierraconnects <alfasierraconnects@gmail.com>
2024-04-03 23:13:25 +0530

    added filter functionality

--- react-restaurant-website/src/components/FoodDisplay/FoodDisplay.js
+++ react-restaurant-website/src/components/FoodDisplay/FoodDisplay.js
@@ -8,16 +8,22 @@ const FoodDisplay = ({ category }) => {
     <div className="mt-8">
       <h2 className="font-semibold text-2xl">Top dishes near you</h2>
       <div className="grid grid-cols-4 mt-8 gap-6 gap-y-12">
-        {food_list.map((item, index) => (
-          <FoodItem
-            key={index}
-            id={item._id}
-            name={item.name}
-            price={item.price}
-            description={item.description}
-            image={item.image}
-          />
-        ))}
+        {food_list.map((item, index) => {
+          // {console.log(category, item.category);}
+          if (category === "All" || category === item.category) {
+            return (
+              <FoodItem
+                key={index}
+                id={item._id}
+                name={item.name}
+                price={item.price}
+                description={item.description}
+                image={item.image}
+              />
+            );
+          }
+          return null;
+        })}
       </div>
     </div>
   );
